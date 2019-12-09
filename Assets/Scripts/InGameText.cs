5a1733e2290b83f97ea17d9e462b71c00ab4327d
KingNullpointer <KingNullpointer@users.noreply.github.com>
2019-12-09 09:19:03 -0500

    Added Proper Text

---
+++ Assets/Scripts/InGameText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameText : MonoBehaviour
+{
+
+    public float timeOnScreen = 3f;
+
+    private Text text;
+    private Text characterText;
+    private Animator anim;
+
+    private bool seen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        characterText = GameObject.Find("UI").GetComponentInChildren<Text>();
+        anim = GameObject.Find("UI").GetComponentInChildren<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !seen) {
+            seen = true;
+
+            anim.Play("dialogueIn");
+
+            characterText.text = text.text;
+
+            StartCoroutine(TextFade());
+        }
+    }
+
+    IEnumerator TextFade()
+    {
+        yield return new WaitForSeconds(timeOnScreen);
+
+        anim.Play("dialogueOut");
+    }
+}
